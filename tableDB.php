29fff6de8a6892ddb75974ff3e253d2774250a49
642067 <36200356+642067@users.noreply.github.com>
2019-02-12 14:35:24 +0000

    Update tableDB.php

--- tableDB.php
+++ tableDB.php
@@ -2,7 +2,28 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "users";
+
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+	die("Connection Failed: " . $conn->connect_error);
+}
+echo "connection successful";
+
+$sql = "CREATE DATABASE theDB";
+if ($conn->query($sql) === TRUE) {
+	echo "database created successfully";
+}else {
+	echo "Error creating database: " . $conn->error;
+}
+?>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "theDB";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,7 +33,7 @@ if ($conn->connect_error) {
 } 
 
 // sql to create table
-$sql = "CREATE TABLE MyGuests (
+$sql = "CREATE TABLE users (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
