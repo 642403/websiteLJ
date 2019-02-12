e95c19466178fe3480b81fa4a13281902bcb0993
642067 <36200356+642067@users.noreply.github.com>
2019-02-12 14:17:40 +0000

    Add files via upload

---
+++ createDB.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+	die("Connection Failed: " . $conn->connect_error);
+}
+echo "connection successful";
+
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+	echo "database created successfully";
+}else {
+	echo "Error creating database: " . $conn->error;
+}
+
+?>
