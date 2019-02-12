d5c104ce4a5c9931f990148c0fed2059feafb654
642067 <36200356+642067@users.noreply.github.com>
2019-02-12 14:20:42 +0000

    Update createDB.php

--- createDB.php
+++ createDB.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
 }
 echo "connection successful";
 
-$sql = "CREATE DATABASE myDB";
+$sql = "CREATE DATABASE users";
 if ($conn->query($sql) === TRUE) {
 	echo "database created successfully";
 }else {
